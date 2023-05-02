3efa22c057809c89504275b4624514ff3de3c35e
MartinMSalas <ingmartinmsalas@gmail.com>
2023-05-01 22:15:53 -0300

    add animated button for recipes

---
+++ DrVirtualFront/dr_virtual/src/components/Button2.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import button2 from "./css/button2.css"
+export const  Button2 = ({children,clickHandler}) => {
+  return (
+    <div>
+
+      <div className="w-full">
+    
+        <button onClick={clickHandler} className="button2 w-full h-12 bg-blue-400">{children}</button>
+      </div>
+
+    </div>
+    );
+}
+ 
